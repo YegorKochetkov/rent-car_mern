100288c7f505a628072079a2b52b957287f6f0c7
Yegor Kochetkov <KochetkovYegorNikolaevich@gmail.com>
2022-10-14 01:44:49 +0300

    Add redux store

---
+++ src/app/store.ts
@@ -0,0 +1,16 @@
+import { Action, configureStore, ThunkAction } from '@reduxjs/toolkit';
+
+export const store = configureStore({
+	reducer: {},
+});
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
+export type AppThunk<ReturnType = void> = ThunkAction<
+	ReturnType,
+	RootState,
+	unknown,
+	Action<string>
+>;
