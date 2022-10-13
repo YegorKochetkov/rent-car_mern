1522beaeabfbae16d9f700c79a83fdafcc25ca99
Yegor Kochetkov <KochetkovYegorNikolaevich@gmail.com>
2022-10-13 20:56:25 +0300

    Add prettier for tailwind classes

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,24 @@ import tw from 'tailwind-styled-components';
 import './App.css';
 
 const Button = tw.button`
+	rounded-md
+	bg-slate-100
+	p-3
 	shadow
 	shadow-gray-400/50
-	hover:shadow
-	hover:shadow-gray-600
-	active:shadow-inner
-	active:shadow-gray-600
-	p-3
 	transition
-	bg-slate-100
-	rounded-md
+	hover:shadow
+	hover:shadow-gray-600/60
+	active:shadow-inner
+	active:shadow-gray-600/60
 `;
 
 function App() {
 	return (
 		<div className="App">
-			<h1 className="text-4xl mb-2 font-bold underline">Hello world!</h1>
+			<h1 className="ml-4 mb-3 p-4 text-4xl font-bold underline">
+				Hello world!
+			</h1>
 			<Button>Super button</Button>
 		</div>
 	);
