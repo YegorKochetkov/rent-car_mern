0a7758595d4cb45ec4511f03de11098a66798bee
Yegor Kochetkov <KochetkovYegorNikolaevich@gmail.com>
2022-10-18 12:46:24 +0300

    Update App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import HomePage from './app/containers/HomePage';
+import HomePage from 'app/containers/HomePage/HomePage';
 import tw from 'twin.macro';
 import './App.css';
 
