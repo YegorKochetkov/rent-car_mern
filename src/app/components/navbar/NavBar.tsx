46948ba89c13d15b4771dbb888669ebc0080673b
Yegor Kochetkov <KochetkovYegorNikolaevich@gmail.com>
2022-10-18 12:45:28 +0300

    Update NavBar

--- src/app/components/navbar/NavBar.tsx
+++ src/app/components/navbar/NavBar.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import tw from 'twin.macro';
 import NavBarMenu from './NavBarMenu';
-import Logo from '../logo/Logo';
+import Logo from '../Logo/Logo';
 
-const NavBarContainer = tw.div`
+const NavBarContainer = tw.menu`
 	flex
 	flex-row
 	items-center
 	justify-between
 	w-full
 	max-w-screen-2xl
-	lg:pr-12
-	lg:pl-12
+	px-2
+	py-1
+	lg:px-12
 `;
 
 function NavBar() {
