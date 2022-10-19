1ef762cc7c87c21ec32e3301c9b389cc9400c071
Yegor Kochetkov <KochetkovYegorNikolaevich@gmail.com>
2022-10-19 17:28:05 +0300

    Improve toggle mobile menu code

--- src/app/components/navbar/NavBarMenu.tsx
+++ src/app/components/navbar/NavBarMenu.tsx
@@ -2,24 +2,28 @@ import React, { useState } from 'react';
 import { useMediaQuery } from 'react-responsive';
 import MenuItems from './MenuItems';
 import tw from 'twin.macro';
+import styled from 'styled-components';
 import { SCREENS } from 'app/helpers/screens';
 
-const MobileMenu = tw.ul`
-	absolute
-	top-10
-	bottom-0
-	left-0
-	right-0
-	flex
-	flex-col
-	items-center
-	gap-4
-	p-3
-	list-none
-	text-4xl
-	bg-white
-	origin-top
-	animate-open-menu
+const MobileMenu = styled.ul<{ isOpen: boolean }>`
+	${tw`
+		hidden
+		absolute
+		top-10
+		bottom-0
+		left-0
+		right-0
+		flex-col
+		items-center
+		gap-4
+		p-3
+		list-none
+		text-4xl
+		bg-white
+		origin-top
+		animate-open-menu
+	`}
+	${(props) => props.isOpen && tw`flex`}
 `;
 
 const Menu = tw.ul`
@@ -30,7 +34,6 @@ const Menu = tw.ul`
 	justify-end
 	gap-5
 	text-2xl
-	bg-white
 `;
 
 const HamburgerButton = tw.button`
@@ -53,11 +56,9 @@ function NavBarMenu() {
 	return isMobile ? (
 		<React.Fragment>
 			<HamburgerButton onClick={toggleMobileMenu}>&#9776;</HamburgerButton>
-			{isOpenMobileMenu && (
-				<MobileMenu>
-					<MenuItems />
-				</MobileMenu>
-			)}
+			<MobileMenu isOpen={isOpenMobileMenu}>
+				<MenuItems />
+			</MobileMenu>
 		</React.Fragment>
 	) : (
 		<Menu>
