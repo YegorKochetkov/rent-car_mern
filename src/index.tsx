ec73b27e9b84550b82e555c77a1ff095794aee92
Yegor Kochetkov <KochetkovYegorNikolaevich@gmail.com>
2022-10-20 11:14:46 +0300

    Move store

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './app/store';
+import { store } from 'app/store/store';
 import { GlobalStyles } from 'twin.macro';
 import './index.css';
 import App from './App';
