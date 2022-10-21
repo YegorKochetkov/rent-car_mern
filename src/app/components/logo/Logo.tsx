4dd318ecd253c12172edd2aacdc5d0f9ff181a29
Yegor Kochetkov <KochetkovYegorNikolaevich@gmail.com>
2022-10-21 18:32:22 +0300

    Update Logo styles

--- src/app/components/logo/Logo.tsx
+++ src/app/components/logo/Logo.tsx
@@ -6,14 +6,15 @@ const LogoContainer = tw.a`
 	flex
 	items-center
 	min-w-fit
-	text-blue-500
-`;
-
-const LogoText = tw.p`
-	text-black
 	text-2xl
-	md:text-3xl
 	font-bold
+	md:text-3xl
+	text-black
+	transition
+	hover:text-red-600
+	[&>span]:transition
+	[&>span]:text-red-600
+	[&>span]:hover:text-black
 `;
 
 const LogoImage = tw.img`
@@ -22,16 +23,11 @@ const LogoImage = tw.img`
 	mr-1
 `;
 
-const AccentSpan = tw.span`text-red-600`;
-
 function Logo() {
 	return (
 		<LogoContainer href="/">
 			<LogoImage src={CarLogoImg} alt="Car shape in circle" />
-
-			<LogoText>
-				Rent<AccentSpan>Car</AccentSpan>.
-			</LogoText>
+			Rent<span>Car</span>.
 		</LogoContainer>
 	);
 }
