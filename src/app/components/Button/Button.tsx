8224cb24c6a9b23d5f366cd078b0d27e8df96c21
Yegor Kochetkov <KochetkovYegorNikolaevich@gmail.com>
2022-10-21 12:29:44 +0300

    Add buttons

---
+++ src/app/components/Button/Button.tsx
@@ -0,0 +1,39 @@
+import React, { ReactNode } from 'react';
+import tw from 'twin.macro';
+
+type ButtonProps = {
+	theme?: 'outlined' | 'filled';
+	children?: ReactNode;
+};
+
+const BaseButton = tw.button`
+	px-5
+	py-3
+	text-lg
+	text-white
+	font-semibold
+	bg-red-600
+	border
+	rounded-md
+	transition
+	shadow
+	hover:shadow-neutral-600
+	active:shadow-inner
+	active::shadow-neutral-600
+`;
+
+const Outlined = tw(BaseButton)`
+	bg-transparent
+	text-black
+	hover:text-white
+	hover::bg-red-600
+`;
+
+function Button(props: ButtonProps) {
+	const { theme, children } = props;
+	if (theme === 'outlined') return <Outlined>{children}</Outlined>;
+
+	return <BaseButton>{children}</BaseButton>;
+}
+
+export default Button;
--- src/app/containers/TopSection.tsx
+++ src/app/containers/TopSection.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Slogan from 'app/components/Slogan/Slogan';
 import McLaren from 'app/components/StandaloneCar/StandaloneCar';
 import tw from 'twin.macro';
+import Button from 'app/components/Button/Button';
 
 const TopSectionContainer = tw.div`
 	flex
@@ -29,11 +30,23 @@ const RightContainer = tw.div`
 	w-1/2
 `;
 
+const AccentSpan = tw.span`text-black hover:text-white`;
+
+const ButtonsContainer = tw.div`flex flex-wrap gap-3 mt-7`;
+
 function TopSection() {
 	return (
 		<TopSectionContainer>
 			<LeftContainer>
 				<Slogan />
+				<ButtonsContainer>
+					<Button>
+						Book your <AccentSpan>Ride</AccentSpan>
+					</Button>
+					<Button theme="outlined">
+						Sell your <AccentSpan>Car</AccentSpan>
+					</Button>
+				</ButtonsContainer>
 			</LeftContainer>
 			<RightContainer>
 				<McLaren />
