d4b43fbe8cf0998c1a1f3ae4ac8b62113be60c8d
Yegor Kochetkov <KochetkovYegorNikolaevich@gmail.com>
2022-10-28 14:31:33 +0300

    add(cars): add new car to list

--- src/app/helpers/cars.ts
+++ src/app/helpers/cars.ts
@@ -2,7 +2,27 @@ import { CarType } from 'app/types/car';
 
 export const Cars: CarType[] = [
 	{
-		name: 'Audi S3 Car',
+		name: 'Rolls-Royce Ghost',
+		mileage: '2k',
+		thumbnail:
+			'https://di-uploads-pod16.dealerinspire.com/competitionbmwofsmithtown/uploads/2021/07/2020-rolls-royce.png',
+		dailyPrice: 150,
+		monthlyPrice: 3600,
+		gearType: 'Auto',
+		gas: 'Petrol',
+	},
+	{
+		name: 'Tesla Model 3',
+		mileage: '13k',
+		thumbnail:
+			'https://images.enervee.com/image/upload/v1551603319/cars/us/img_4155044801.png',
+		dailyPrice: 90,
+		monthlyPrice: 2200,
+		gearType: 'Auto',
+		gas: 'electric',
+	},
+	{
+		name: 'Audi S3',
 		mileage: '10k',
 		thumbnail:
 			'https://cdn.jdpower.com/Models/640x480/2017-Audi-S3-PremiumPlus.jpg',
@@ -12,37 +32,26 @@ export const Cars: CarType[] = [
 		gas: 'Petrol',
 	},
 	{
-		name: 'Audi S3 Car 2',
+		name: 'BMW M2',
 		mileage: '10k',
-		thumbnail:
-			'https://cdn.jdpower.com/Models/640x480/2017-Audi-S3-PremiumPlus.jpg',
+		thumbnail: 'https://pbs.twimg.com/media/EmD1MyRXgAENrUW.jpg',
 		dailyPrice: 70,
 		monthlyPrice: 1600,
 		gearType: 'Auto',
 		gas: 'Petrol',
 	},
 	{
-		name: 'Honda City 5 Seater Car ducimus sunt placeat praesentium',
+		name: 'Honda CR-V',
 		mileage: '20k',
 		thumbnail:
-			'https://shinewiki.com/wp-content/uploads/2019/11/honda-city.jpg',
-		dailyPrice: 50,
-		monthlyPrice: 1500,
-		gearType: 'Auto',
-		gas: 'Petrol',
-	},
-	{
-		name: 'Audi S3 Car ducimus sunt placeat praesentium',
-		mileage: '10k',
-		thumbnail:
-			'https://cdn.jdpower.com/Models/640x480/2017-Audi-S3-PremiumPlus.jpg',
-		dailyPrice: 70,
+			'https://static.cargurus.com/images/forsale/2018/02/14/10/12/2017_honda_cr-v-pic-2561454830775230295-1024x768.jpeg?io=true&width=640&height=480&fit=bounds&format=jpg&auto=webp',
+		dailyPrice: 60,
 		monthlyPrice: 1600,
 		gearType: 'Auto',
 		gas: 'Petrol',
 	},
 	{
-		name: 'Honda City 5 Seater Car 2',
+		name: 'Honda City 5 Seater',
 		mileage: '20k',
 		thumbnail:
 			'https://shinewiki.com/wp-content/uploads/2019/11/honda-city.jpg',
@@ -52,12 +61,22 @@ export const Cars: CarType[] = [
 		gas: 'Petrol',
 	},
 	{
-		name: 'Honda City 5 Seater Car 3',
+		name: 'Jeep Wrangler',
 		mileage: '20k',
 		thumbnail:
-			'https://shinewiki.com/wp-content/uploads/2019/11/honda-city.jpg',
-		dailyPrice: 50,
-		monthlyPrice: 1500,
+			'https://di-uploads-pod11.dealerinspire.com/newbergdodgechryslerjeep/uploads/2018/12/HP_ModelSlide_640x480_2_JeepWrangler.png',
+		dailyPrice: 60,
+		monthlyPrice: 1700,
+		gearType: 'Auto',
+		gas: 'Diesel',
+	},
+	{
+		name: 'Lamborghini Urus',
+		mileage: '5k',
+		thumbnail:
+			'https://mractravel.blob.core.windows.net/mrac-media/2019/05/LamborghiniUrus.png',
+		dailyPrice: 150,
+		monthlyPrice: 3800,
 		gearType: 'Auto',
 		gas: 'Petrol',
 	},
