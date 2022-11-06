dc3a8bc1a19f290544d7f0f98afba2b260f5d5ad
Yegor Kochetkov <KochetkovYegorNikolaevich@gmail.com>
2022-11-07 00:49:39 +0200

    feat(BookingSteps): add carousel for steps
    on small screens booking steps shown as carousel

--- src/app/containers/BookingSteps.tsx
+++ src/app/containers/BookingSteps.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
 	faCalendarAlt,
 	faCarSide,
@@ -6,12 +6,17 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 import tw from 'twin.macro';
 import BookingStep from 'app/components/BookingStep/BookingStep';
+import Carousel, { autoplayPlugin, Dots } from '@brainhubeu/react-carousel';
+import '@brainhubeu/react-carousel/lib/style.css';
+import { useMediaQuery } from 'react-responsive';
+import { SCREENS } from 'app/helpers/screens';
 
 const Wrapper = tw.div`
 	w-full
 	flex
 	flex-col
 	items-center
+	gap-7
 	mb-16
 	py-3
 	lg:py-6
@@ -27,29 +32,63 @@ const Title = tw.h2`
 
 const StepsContainer = tw.div`
 	flex
+	flex-col
+	sm:flex-row
 	justify-evenly
-	gap-6
-	flex-wrap
-	mt-7
+	gap-5
+	sm:gap-0
 	lg:mt-16
 `;
 
+const steps = [
+	<BookingStep icon={faMapMarkedAlt} title="Choose Location">
+		Find the nearest <span>Rent</span>Car<span>.</span> point and book your car.
+	</BookingStep>,
+	<BookingStep icon={faCalendarAlt} title="Pick-Up Date">
+		Pickup the Best Date to rent a car for you.
+	</BookingStep>,
+	<BookingStep icon={faCarSide} title="Book Your Car">
+		Book your nice car with ease in one single click.
+	</BookingStep>,
+];
+
 function BookingSteps() {
+	const isMobile = useMediaQuery({ maxWidth: SCREENS.sm });
+	const [currentSlide, setCurrentSlide] = useState(0);
+
 	return (
 		<Wrapper>
 			<Title>Our working steps</Title>
-			<StepsContainer>
-				<BookingStep icon={faMapMarkedAlt} title="Choose Location">
-					Find the nearest <span>Rent</span>Car<span>.</span> point and book
-					your car.
-				</BookingStep>
-				<BookingStep icon={faCalendarAlt} title="Pick-Up Date">
-					Pickup the Best Date to rent a car for you.
-				</BookingStep>
-				<BookingStep icon={faCarSide} title="Book Your Car">
-					Book your nice car with ease in one single click.
-				</BookingStep>
-			</StepsContainer>
+			{isMobile ? (
+				<React.Fragment>
+					<Carousel
+						value={currentSlide}
+						onChange={setCurrentSlide}
+						slides={[...steps]}
+						plugins={[
+							'infinite',
+							{
+								resolve: autoplayPlugin,
+								options: {
+									interval: 2500,
+								},
+							},
+						]}
+						animationSpeed={1000}
+					/>
+					<Dots
+						value={currentSlide}
+						onChange={setCurrentSlide}
+						number={steps.length}
+					/>
+				</React.Fragment>
+			) : (
+				<StepsContainer>
+					{steps.map((step, index) => (
+						<div key={index}>{step}</div>
+					))}
+				</StepsContainer>
+			)}
 		</Wrapper>
 	);
 }
